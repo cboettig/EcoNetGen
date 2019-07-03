f995b8389063a0b66104119b7572aa40ed6d95f8
Carl Boettiger <cboettig@gmail.com>
2019-07-03 14:05:14 -0700

    include required black magic, / ht @coatless :tophat:

--- src/zzz.c
+++ src/zzz.c
@@ -2,6 +2,9 @@
 #include <R_ext/Rdynload.h>
 #include <Rversion.h>
 
+#define USE_FC_LEN_T
+#include <Rconfig.h>
+
 void F77_SUB(rndstart)(void) { GetRNGstate(); }
 void F77_SUB(rndend)(void) { PutRNGstate(); }
 double F77_SUB(unifrnd)(void) { return unif_rand(); }
